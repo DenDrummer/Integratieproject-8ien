31d19f83a546b7ab34cc0b96097f10f94448ea3a
DenDrummer <muzikant96@gmail.com>
2018-03-12 17:37:37 +0100

    minor package foutje opgelost

--- UI-MVC/Global.asax.cs
+++ UI-MVC/Global.asax.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace V2
+namespace IP_8IEN.UI.MVC
 {
     public class MvcApplication : System.Web.HttpApplication
     {
