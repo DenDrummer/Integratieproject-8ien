7f98ab6c9490023088fcf51737c4e629ff6f9b52
Sam <Sam@LENOVO-PC>
2018-03-15 14:52:01 +0100

    Revert "just wa aanpassingskes"
    This reverts commit 71d2f9306dfb6ebf747d2090f5d8bd71a529c8e7.

--- Domain/Gebruikers/Gebruiker.cs
+++ Domain/Gebruikers/Gebruiker.cs
@@ -20,6 +20,5 @@ namespace IP_8IEN.BL.Domain.Gebruikers
 
         public ICollection<AlertInstelling> AlertInstellingen { get; private set; }
         public ICollection<Dashboard> Dashboards { get; private set; }
-        
     }
 }
