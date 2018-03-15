7d44435187ca61b57d6547330169110bbfcce005
DenDrummer <muzikant96@gmail.com>
2018-03-15 15:57:21 +0100

    start testing rond uitlezen en parsen JSON

---
+++ JSONTesting/Program.cs
@@ -0,0 +1,18 @@
+﻿using IP_8IEN.BL.Domain.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message msg = JsonConvert.DeserializeObject<Message>("?"); //vervang met link naar json?
+        }
+    }
+}
