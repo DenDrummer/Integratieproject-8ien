eb5ac9af1ac40dd5159784e111dbd540b0453e3c
ivaylodeshev <32093566+ivaylodeshev@users.noreply.github.com>
2018-03-16 11:42:12 +0100

    version1

---
+++ BL/IDataManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using IP_8IEN.BL.Domain.Data;
+
+namespace IP_8IEN.BL
+{
+    public interface IDataManager
+    {
+        void AddMessages(string sourceUrl);
+        Message GetMessage(int messageId);
+    }
+}
