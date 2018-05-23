7bc4a6abdccabc60b2805ac2f1ed3148ea9d507f
Thomas De Witte <thomas.dewitte@student.kdg.be>
2018-05-23 14:02:42 +0200

    toevoegensuperadmin

---
+++ IP3_8IEN/MVC-S/Controllers/SuperadminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_S.Controllers
+{
+    public class SuperadminController : Controller
+    {
+        // GET: Superadmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Adminlijst()
+        {
+            return View();
+        }
+
+        public ActionResult Inloggen()
+        {
+            return View();
+        }
+
+        public ActionResult SMBeheren()
+        {
+            return View();
+        }
+    } 
+
+}
